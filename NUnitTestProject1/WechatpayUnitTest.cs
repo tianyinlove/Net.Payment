ed4c1c2264fa218ac300de34f313afeedd531fc2
tianyin <tianyin0910@emoney.cn>
2020-07-29 11:41:38 +0800

    更新文档

---
+++ NUnitTestProject1/WechatpayUnitTest.cs
@@ -0,0 +1,185 @@
+﻿using Net.Wechatpay;
+using Net.Wechatpay.Constants;
+using Net.Wechatpay.Request;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestProject1
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WechatpayUnitTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public async Task Test1()
+        {
+            var config = new WechatpayConfig
+            {
+                NotifyUrl = "",
+                AppId = "",
+                MchId = "",
+                SignKey = ""
+            };
+
+            var request = new WechatTradeAppPayRequest
+            {
+                TotalFee = 100,
+                OutTradeNo = "202007297777",
+                Body = "产品",
+                Detail = "产品",
+                TimeExpire = DateTime.Now.AddDays(15).ToString("yyyyMMddHHmmss"),
+                TradeType = WechatConstants.APP,
+                TimeStart = DateTime.Now.ToString("yyyyMMddHHmmss"),
+                NotifyUrl = config.NotifyUrl
+            };
+
+            var response = await WechatpayClient.CreateOrderAsync(request, config);
+
+            if (string.IsNullOrEmpty(response.Body))
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.Pass();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public async Task Test2()
+        {
+            var config = new WechatpayConfig
+            {
+                NotifyUrl = "",
+                AppId = "",
+                MchId = "",
+                SignKey = ""
+            };
+
+            var request = new WechatTradeQueryRequest
+            {
+                OutTradeNo = "202007297777"
+            };
+
+            var response = await WechatpayClient.OrderQueryAsync(request, config);
+
+            if (string.IsNullOrEmpty(response.TransactionId))
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.Pass();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public async Task Test5()
+        {
+            var config = new WechatpayConfig
+            {
+                NotifyUrl = "",
+                AppId = "",
+                MchId = "",
+                SignKey = ""
+            };
+
+            var request = new WechatRefundRequest
+            {
+                OutTradeNo = "202007297777",
+                TotalFee = 100,
+                RefundFee = 100,
+                OutRefundNo = WechatService.GenerateOutTradeNo(config)
+            };
+
+            var response = await WechatpayClient.RefundAsync(request, config);
+
+            if (string.IsNullOrEmpty(response.TransactionId))
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.Pass();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public async Task Test6()
+        {
+            var config = new WechatpayConfig
+            {
+                NotifyUrl = "",
+                AppId = "",
+                MchId = "",
+                SignKey = ""
+            };
+
+            var request = new WechatTradeCloseRequest
+            {
+                OutTradeNo = "202007297777"
+            };
+
+            var response = await WechatpayClient.CloseOrderAsync(request, config);
+
+            if (string.IsNullOrEmpty(response.ReturnCode))
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.Pass();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void Test4()
+        {
+            var data = "";//xml请求数据
+            var request = new WechatpayData();
+            request.FromXml(data);
+
+            var config = new WechatpayConfig
+            {
+                NotifyUrl = "",
+                AppId = "",
+                MchId = "",
+                SignKey = ""
+            };
+            var response = WechatpayClient.Notify(request, config);
+
+            if (string.IsNullOrEmpty(response.OutTradeNo))
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.Pass();
+            }
+        }
+    }
+}
